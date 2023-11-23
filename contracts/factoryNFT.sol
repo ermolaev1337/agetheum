b54bfb6169f665ef89db5692c0b53b7c75a64695
Egor Ermolaev <egor.ermolaev@uni.lu>
2023-11-23 13:30:44 +0100

    Implement Farmer and Certifier

--- contracts/factoryNFT.sol
+++ contracts/factoryNFT.sol
@@ -2,19 +2,49 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract FactoryNFT is ERC721URIStorage {
 
+contract FactoryNFT is AccessControl, ERC721URIStorage {
     uint256 private _tokenIdCounter;
 
-    constructor() ERC721("Factory NFT", "FTN") {
+//  Role-based access control
+    bytes32 public constant FARMER_ROLE = keccak256("FARMER_ROLE");
+    bytes32 public constant CERTIFIER_ROLE = keccak256("CERTIFIER_ROLE");
 
+    constructor() ERC721("Agricultural Certification Token", "ACT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function createToken(string memory tokenURI) public returns (uint) {
-        _mint(msg.sender, _tokenIdCounter);
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl, ERC721URIStorage) returns (bool){
+        return super.supportsInterface(interfaceId);
+    }
+
+    function grantFarmerRole(address _address) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(FARMER_ROLE, _address);
+    }
+
+    function grantCertifierRole(address _address) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(CERTIFIER_ROLE, _address);
+    }
+
+    mapping(address => string) public addressToTokenURI;//TODO deal with array of tokenURIs for each address (farmer)
+
+    function requestCertification(string memory tokenURI) public onlyRole(FARMER_ROLE) {
+        addressToTokenURI[msg.sender] = tokenURI;
+    }
+
+    function approveCertification(address farmerAddress) public onlyRole(CERTIFIER_ROLE) returns (uint) {//TODO reentrancy guard
+        uint _tokenCounter = createToken(farmerAddress, addressToTokenURI[farmerAddress]);
+        delete addressToTokenURI[farmerAddress];
+
+        return _tokenCounter;
+    }
+
+    function createToken(address _holder, string memory tokenURI) private returns (uint) {
+        _mint(_holder, _tokenIdCounter);
         _setTokenURI(_tokenIdCounter, tokenURI);
 
-        return _tokenIdCounter++;
+        return ++_tokenIdCounter;
     }
 }
--- test/factoryNFT.js
+++ test/factoryNFT.js
@@ -1,25 +1,69 @@
-const { expect } = require("chai");
+const {expect} = require("chai");
 const hre = require("hardhat");
+// const {
+//     loadFixture,
+// } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
-describe("Minting the token and returning it", function () {
-    it("should the contract be able to mint a function and return it", async function () {
-        const metadata = "https://opensea-creatures-api.herokuapp.com/api/creature/1"; // Random metadata URL
 
-        // Deploy the contract. Ensure that the FactoryNFT contract is correctly written and can be deployed
+describe("FactoryNFT", function () {
+    const deployContract = async () => {
+        const [_owner, _farmer, _certifier] = await ethers.getSigners();
+
         const FactoryNFTContract = await ethers.getContractFactory("FactoryNFT");
         const factoryNFT = await FactoryNFTContract.deploy();
 
-        // Mint the token and wait for the transaction to complete
-        const transaction = await factoryNFT.createToken(metadata); // Minting the token
-        const tx = await transaction.wait(); // Waiting for the token to be minted
+        const tx1 = await factoryNFT.connect(_owner).grantFarmerRole(_farmer.address);
+        await tx1.wait();
 
-        // Extract the token ID from the transaction event
-        const event = tx.events[0];
-        const value = event.args[2];
-        const tokenId = value.toNumber(); // Getting the tokenID
+        const tx2 = await factoryNFT.connect(_owner).grantCertifierRole(_certifier.address);
+        await tx2.wait();
 
-        // Retrieve the token URI and compare it with the metadata
-        const tokenURI = await factoryNFT.tokenURI(tokenId); // Using the tokenURI from ERC721 to retrieve the metadata
-        expect(tokenURI).to.be.equal(metadata); // Comparing and testing
+        return [factoryNFT, _owner, _farmer, _certifier]
+    }
+
+
+    describe("Request and receive certification", async () => {
+
+        it("farmer should request certification, certifier should confirm it", async () => {
+
+            const metadata = JSON.stringify({
+                "description": "Farmer ACT",
+                "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Vehn%C3%A4pelto_6.jpg/1599px-Vehn%C3%A4pelto_6.jpg",
+                "name": "Wheat",
+                "attributes": [
+                    {name: "Composts and Weed Suppressants", value: ["Original Potting Plus Mix", "Vegro Seed Mix"]},
+                    {name: "Fertilisers", value: ["Meadowsalt", "EPSO Combitop", "Zinc"]},
+                    {name: "Microorganisms", value: ["SlurryBugs Shift It", "Nutri-Life Tricho-Shield"]},
+                    {name: "Supplements", value: ["P870 Tracesure Lamb"]},
+                ]
+            })
+
+            const [factoryNFT, owner, farmer, certifier] = await deployContract();
+
+            const requestCertificationTx = await factoryNFT.connect(farmer).requestCertification(metadata);
+            await requestCertificationTx.wait();
+
+            const dictBefore = await factoryNFT.connect(farmer).addressToTokenURI(farmer.address)
+            expect(metadata).to.be.equal(dictBefore);
+
+            const approveCertificationTx = await factoryNFT.connect(certifier).approveCertification(farmer.address);
+            const approveCertificationTxMined = await approveCertificationTx.wait();
+            // console.debug("approveCertificationTx", approveCertificationTx)
+            // expect(approveCertificationTx).to.be.equal(1)
+
+            const dictAfter = await factoryNFT.connect(farmer).addressToTokenURI(farmer.address)
+            expect("").to.be.equal(dictAfter);
+
+            // // Extract the token ID from the transaction event
+            const event = approveCertificationTxMined.events[0];
+            const value = event.args[2];
+            const tokenId = value.toNumber(); // Getting the tokenID
+
+            // Retrieve the token URI and compare it with the metadata
+            const tokenURI = await factoryNFT.tokenURI(tokenId); // Using the tokenURI from ERC721 to retrieve the metadata
+            expect(tokenURI).to.be.equal(metadata); // Comparing and testing
+
+        });
     });
 });
+
